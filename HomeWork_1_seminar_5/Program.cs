4082e506558d79fd6949cbf8ec736aaa42053399
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-29 23:03:01 +0500

    create code_1

---
+++ HomeWork_1_seminar_5/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] massiv = new int[999];
+
+Random rnd = new Random();
+
+for (int i = 0; i < massiv.Length; i++)
+{
+    massiv[i] = rnd.Next(100,1000);
+    System.Console.Write(massiv[i] + " ");
+}
+System.Console.WriteLine();
+
+int count = 0;
+
+foreach (var item in massiv)
+{
+    if (item % 2 == 0)
+    {
+        count++;
+    }
+   
+}
+
+// for (int i = 0; i < massiv.Length; i++)
+// {
+//     if (massiv[i]% 2 ==0)
+//     {
+//         count=count+1;
+//     }
+    
+// }
+System.Console.Write("\n Количество четных чисел в массиве: " + count);
