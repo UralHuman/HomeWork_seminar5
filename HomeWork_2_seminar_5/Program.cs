38d4e963d1ec92d8f65189206ef2e63ba8ddd478
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-30 12:42:53 +0500

    create code_2

---
+++ HomeWork_2_seminar_5/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.Write(">>> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] massiv = new int[n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    massiv[i] = rnd.Next(-100,100);
+    System.Console.Write(massiv[i] + " ");
+}
+
+int sum =0;
+
+for (int i = 0; i < n; i++)
+{
+    if (i%2!=0)
+    {
+        sum=sum+massiv[i];
+    }
+    
+}
+System.Console.WriteLine("\n Сумма = " + sum);
+
