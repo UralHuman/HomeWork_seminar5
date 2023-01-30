57deeec1fc7135712b24f66079134384da735705
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-30 16:00:58 +0500

    create code_3

---
+++ HomeWork_3_seminar_5/Program.cs
@@ -0,0 +1,30 @@
+﻿double[] massiv = new double[10];    
+Random rnd = new Random();
+
+double minNum = double.MaxValue;
+double maxNum = double.MinValue;
+
+for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = rnd.Next(1,1000);
+        System.Console.Write(massiv[i] + " ");  
+    }
+
+System.Console.WriteLine();
+
+foreach (var item in massiv)
+{
+    if (item<minNum)
+    {
+        minNum = item;
+    }
+    else if (item>maxNum)
+    {
+        maxNum = item;
+    }
+
+    
+}
+System.Console.WriteLine($"\nМаксимальный элемент массива: ({maxNum})\nМинимальный элемент массива: ({minNum})");
+System.Console.WriteLine($"\nРазница между максимальным и минимальным элементом массива: ({maxNum-minNum})");
+
